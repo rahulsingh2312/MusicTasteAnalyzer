9f082d55936b5e57bd51425737c9a8a2eb9aa04e
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2023-11-11 22:08:25 +0530

    vercol foxesl

--- src/script.js
+++ src/script.js
@@ -25,7 +25,8 @@ export async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "https://music-taste-analyzer.vercel.app/callback");
+    params.append("redirect_uri", encodeURIComponent("https://music-taste-analyzer.vercel.app/callback"));
+
     params.append("scope", "user-read-private user-read-email user-top-read");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -60,7 +61,8 @@ export async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "https://music-taste-analyzer.vercel.app/callback");
+    params.append("redirect_uri", encodeURIComponent("https://music-taste-analyzer.vercel.app/callback"));
+
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
